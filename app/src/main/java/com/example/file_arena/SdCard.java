c5f16b1ac0b814f3b8e82dc25e994c0894d11a6b
MEHEDI-118 <mhsami53oleander@gmail.com>
2019-03-13 19:56:30 +0600

    Show All External storage files and folder & subfolder. open files using default app

--- app/src/main/java/com/example/file_arena/SdCard.java
+++ app/src/main/java/com/example/file_arena/SdCard.java
@@ -1,13 +1,86 @@
 package com.example.file_arena;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.net.URLConnection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SdCard extends AppCompatActivity {
+
+    ListView lview;
+
+    public static String[] getStorageDirectories(Context pContext) {
+        // Final set of paths
+        final Set<String> rv = new HashSet<>();
+
+        //Get primary & secondary external device storage (internal storage & micro SDCARD slot...)
+        File[] listExternalDirs = ContextCompat.getExternalFilesDirs(pContext, null);
+        for (int i = 0; i < listExternalDirs.length; i++) {
+            if (listExternalDirs[i] != null) {
+                String path = listExternalDirs[i].getAbsolutePath();
+                int indexMountRoot = path.indexOf("/Android/data/");
+                if (indexMountRoot >= 0 && indexMountRoot <= path.length()) {
+                    //Get the root path for the external directory
+                    rv.add(path.substring(0, indexMountRoot));
+                }
+            }
+        }
+        return rv.toArray(new String[rv.size()]);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sd_card);
+
+        lview = findViewById(R.id.lview1);
+        String[] list = getStorageDirectories(SdCard.this);
+
+        File f = new File(list[0]);
+        final File[] paths = f.listFiles();
+
+        String[] values = f.list();//getting the list of files in string array
+        //now presenting the data into screen
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, values);
+        lview.setAdapter(adapter);//setting the adapter
+        lview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String s = paths[position].getAbsolutePath();
+
+                File f = new File(s);
+
+                if (f.isDirectory()) {
+                    Intent myIntent = new Intent(SdCard.this, SubfolderView.class);
+                    myIntent.putExtra("subfolder", s);
+                    startActivity(myIntent);
+                    Toast.makeText(SdCard.this, s, Toast.LENGTH_SHORT).show();
+                } else {
+                    // MimeTypeMap myMime=MimeTypeMap.getSingleton();
+                    Intent newIntent = new Intent(Intent.ACTION_VIEW);
+
+                    newIntent.setDataAndType(Uri.fromFile(f), URLConnection.guessContentTypeFromName(s));
+
+                    Intent j = Intent.createChooser(newIntent, "Choose an application to open with: ");
+                    startActivity(j);
+                }
+
+            }
+        });
+
     }
 }
