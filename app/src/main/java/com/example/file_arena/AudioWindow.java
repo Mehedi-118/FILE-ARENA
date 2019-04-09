1d1dc72072a92aa6b7a771b8ad33663e60ab575e
MEHEDI-118 <mhsami53oleander@gmail.com>
2019-04-09 19:29:33 +0600

    audio window

--- app/src/main/java/com/example/file_arena/AudioWindow.java
+++ app/src/main/java/com/example/file_arena/AudioWindow.java
@@ -2,8 +2,11 @@ package com.example.file_arena;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -17,6 +20,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class AudioWindow extends AppCompatActivity {
@@ -28,6 +33,9 @@ public class AudioWindow extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#FF0000"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
         setContentView(R.layout.activity_audio_window);
 
         if (ContextCompat.checkSelfPermission(AudioWindow.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -46,38 +54,68 @@ public class AudioWindow extends AppCompatActivity {
     public void result() {
         listView = findViewById(R.id.audioListid);
         arrayList = new ArrayList<>();
-        getMusic();
+
+        final File[] paths = getMusic();
+
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                String s = paths[position].getAbsolutePath();
+                File ff = new File(s);
+                //final File[] paths = f.listFiles();
+                Toast.makeText(AudioWindow.this, s, Toast.LENGTH_SHORT).show();
+                Intent newIntent = new Intent(Intent.ACTION_VIEW);
 
+                newIntent.setDataAndType(Uri.fromFile(ff), URLConnection.guessContentTypeFromName(s));
+
+                Intent j = Intent.createChooser(newIntent, "Choose an application to open with: ");
+                startActivity(j);
             }
         });
     }
 
-    public void getMusic() {
+    public File[] getMusic() {
+        File[] path = new File[0];
+
         ContentResolver contentResolver = getContentResolver();
         Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+        String s = songUri.toString();
+        File f = new File(s);
+
+
         Cursor songCursor = contentResolver.query(songUri, null, null, null, null);
 
         if (songCursor != null && songCursor.moveToFirst()) {
             int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            String[] filePathColumn = {MediaStore.Audio.Media.DATA};
 
+            int i = 0;
             do {
                 String currentTitle = songCursor.getString(songTitle);
                 String currentArtist = songCursor.getString(songArtist);
                 arrayList.add(currentTitle + "\n" + currentArtist);
+                int columnIndex = songCursor.getColumnIndex(filePathColumn[0]);
+                String yourRealPath = songCursor.getString(columnIndex);
+                File ff = new File(yourRealPath);
 
-
+                i++;
             } while (songCursor.moveToNext());
         }
+        Uri uri = songUri;
+        String[] filePathColumn = {MediaStore.Audio.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+
+
+        cursor.close();
+        return path;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSION_REQUEST: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -95,3 +133,4 @@ public class AudioWindow extends AppCompatActivity {
         }
     }
 }
+
