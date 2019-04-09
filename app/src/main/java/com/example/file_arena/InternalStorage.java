618da7a3d43ed5a4712d60e1077fecd3bda48171
MEHEDI-118 <mhsami53oleander@gmail.com>
2019-04-09 19:30:57 +0600

    menuItem added &long clicked listener

--- app/src/main/java/com/example/file_arena/InternalStorage.java
+++ app/src/main/java/com/example/file_arena/InternalStorage.java
@@ -1,13 +1,23 @@
 package com.example.file_arena;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,10 +26,16 @@ import java.net.URLConnection;
 
 public class InternalStorage extends AppCompatActivity {
     ListView lview;
+    int count = 0;
+    String[] P = new String[0];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#FF0000"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
         setContentView(R.layout.activity_internal_storage);
+
 
         lview = findViewById(R.id.internal_storage);
         final String path = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -56,75 +72,156 @@ public class InternalStorage extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
-
-       /* lview.setAdapter(new ListAdapter() {
+        /**Item On Long clicked  Start*/
+        lview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        lview.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
             @Override
-            public boolean areAllItemsEnabled() {
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                count++;
+
+                mode.setTitle(count + " Item is selected");
+
+
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                MenuInflater menuInflater = mode.getMenuInflater();
+                menuInflater.inflate(R.menu.long_clicked_menu_item, menu);
+
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
                 return false;
             }
 
             @Override
-            public boolean isEnabled(int position) {
-                return false;
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.ShareId: {
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        startActivity(Intent.createChooser(intent, "Share With"));
+                        return true;
+
+                    }
+                    case R.id.Deleteid: {
+
+                    }
+
+
+                    default:
+                        return false;
+                }
+
             }
 
             @Override
-            public void registerDataSetObserver(DataSetObserver observer) {
+            public void onDestroyActionMode(ActionMode mode) {
 
-            }
-
-            @Override
-            public void unregisterDataSetObserver(DataSetObserver observer) {
-
-            }
-
-            @Override
-            public int getCount() {
-                return paths.length;
-            }
-
-            @Override
-            public Object getItem(int position) {
-                return paths[position];
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return false;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                convertView = getLayoutInflater().inflate(R.layout.activity_subfolder_view, parent, false);
-                ListView listView = convertView.findViewById(R.id.sublist);
-                listView.(Uri.parse(getItem(position).toString()));
-            }
-
-            @Override
-            public int getItemViewType(int position) {
-                return 0;
-            }
-
-            @Override
-            public int getViewTypeCount() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
             }
         });
-*/
+
+
+        /** Item Onlongclicked  End  */
+
     }
+
+    @Override
+
+    /** MENU ITEM WITH LISTENER */
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.newFolderId: {
+                newFolder();
+                return true;
+            }
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void newFolder() {
+        final String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(InternalStorage.this);
+        View mView = getLayoutInflater().inflate(R.layout.floating_edit_textwindow, null);
+        final EditText FileName = mView.findViewById(R.id.write);
+        Button Done = mView.findViewById(R.id.DoneID);
+        Done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (FileName.getText().toString().isEmpty()) {
+
+                    File theDir = new File(path + "//new folder");
+                    if (!theDir.exists()) {
+                        try {
+                            theDir.mkdir();
+                            Toast.makeText(InternalStorage.this, "New Folder created", Toast.LENGTH_SHORT).show();
+                            InternalStorage.this.finish();
+                            Intent InternalStorage = new Intent(InternalStorage.this, InternalStorage.class);
+                            startActivity(InternalStorage);
+
+                        } catch (SecurityException se) {
+                            Toast.makeText(InternalStorage.this, "Cannot create folder here", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        int i = 0;
+                        while (true) {
+                            File theDirx = new File(path + "//new folder" + "(" + (++i) + ")");
+                            if (!theDirx.exists()) {
+                                try {
+                                    theDirx.mkdir();
+                                    Toast.makeText(InternalStorage.this, "new folder" + "(" + (i) + ")" + " created", Toast.LENGTH_SHORT).show();
+                                    InternalStorage.this.finish();
+                                    Intent InternalStorage = new Intent(InternalStorage.this, InternalStorage.class);
+                                    startActivity(InternalStorage);
+                                    break;
+                                } catch (SecurityException se) {
+                                    Toast.makeText(InternalStorage.this, "Cannot create folder here", Toast.LENGTH_SHORT).show();
+                                    break;
+                                }
+                            }
+
+                        }
+
+                    }
+                } else {
+                    String folderName = FileName.getText().toString();
+                    File theDir = new File(path + "//" + folderName);
+                    if (!theDir.exists()) {
+                        try {
+                            theDir.mkdir();
+                            Toast.makeText(InternalStorage.this, "\"" + folderName + "\" folder created", Toast.LENGTH_SHORT).show();
+                            InternalStorage.this.finish();
+                            Intent InternalStorage = new Intent(InternalStorage.this, InternalStorage.class);
+                            startActivity(InternalStorage);
+
+                        } catch (SecurityException se) {
+                            Toast.makeText(InternalStorage.this, "Cannot create folder here", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(InternalStorage.this, "A folder named \"" + folderName + "\" already exists", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+
+            }
+        });
+        mBuilder.setView(mView);
+        AlertDialog alertDialog = mBuilder.create();
+        alertDialog.show();
+    }
+
 }
+
