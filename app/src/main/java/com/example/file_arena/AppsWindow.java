8fdff429007859db30399233789823c8595eb3cc
MEHEDI-118 <mhsami53oleander@gmail.com>
2019-04-13 13:34:54 +0600

    apps screen

--- app/src/main/java/com/example/file_arena/AppsWindow.java
+++ app/src/main/java/com/example/file_arena/AppsWindow.java
@@ -1,5 +1,7 @@
 package com.example.file_arena;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,6 +10,9 @@ public class AppsWindow extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#FF0000"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
         setContentView(R.layout.activity_apps_window);
     }
 }
